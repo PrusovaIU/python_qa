cc08060f3224f90f8bfc04d66a656d473d88dd7b
prusova_y <y-prusova@gmail.com>
2021-07-16 11:03:20 +0300

    New: square

---
+++ src/square.py
@@ -0,0 +1,10 @@
+from .figure import Figure
+
+class Square(Figure):
+    name = "square"
+
+    def __init__(self, side: float):
+        super(Square, self).__init__([side])
+
+    def area(self) -> float:
+        return self.sides[0] ** 2
